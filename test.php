585b4e91d0588cb71cb5d1060d7642184b173187
nobu <nobu@192.168.11.39>
2015-08-28 12:25:37 +0900

    test02

---
+++ test.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head></head>
+    <body><pre>
+        <?php
+        for($x=0; $x<10; $x++){
+            echo "[".$x."]\n";
+        }
+        ?>
+        </pre>
+        
+    </body>   
+</html>
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
